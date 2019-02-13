8fe7bb6a1cba7f6621d9af3b487e30094eaf1e12
Chris Dosé <chris.dose@gmail.com>
2019-02-12 19:59:06 -0800

    update a few more dependencies

--- mix.exs
+++ mix.exs
@@ -64,9 +64,9 @@ defmodule Excal.MixProject do
       {:benchee, "~> 0.13", only: :dev, runtime: false},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:elixir_make, "~> 0.4", runtime: false},
       {:ex_doc, "~> 0.18", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.9", only: :test, runtime: false},
-      {:elixir_make, "~> 0.4", runtime: false}
+      {:excoveralls, "~> 0.9", only: :test, runtime: false}
     ]
   end
 end
