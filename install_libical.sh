0c11241463cb52311311e132d02539243f0ebdc0
Chris Dosé <chris.dose@gmail.com>
2019-03-11 21:46:13 -0700

    remove need for docker in ci

---
+++ install_libical.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# This script is intended only to be used in CI environments, don't use it to install libical on your system!
+
+LIBICAL_VERSION="v3.0.4"
+
+git clone --branch $LIBICAL_VERSION https://github.com/libical/libical.git
+mkdir libical/build
+
+cd libical/build
+
+cmake -DWITH_CXX_BINDINGS=false \
+  -DICAL_BUILD_DOCS=false \
+  -DICAL_GLIB=false \
+  -DCMAKE_RELEASE_TYPE=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DSHARED_ONLY=true \
+  ..
+
+make
+make install
