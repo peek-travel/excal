33e4c1ae1d213c10dc4627797ef841dd0b7385eb
Chris Dosé <chris.dose@gmail.com>
2019-12-15 15:02:30 -0800

    update CI versions

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Excal.MixProject do
       app: :excal,
       compilers: [:elixir_make] ++ Mix.compilers(),
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
